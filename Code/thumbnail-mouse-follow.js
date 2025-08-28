abdf0757f7c9b2abdf1f80c2dbf57560d65b78c0
Yotam Rozin <yotam.rozin@gmail.com>
2025-08-28 14:37:27 +0100

    changed move left right max variables

--- Code/thumbnail-mouse-follow.js
+++ Code/thumbnail-mouse-follow.js
@@ -13,13 +13,13 @@ document.addEventListener("DOMContentLoaded", function() {
     smoothingFactor: 0.15, // How smooth the rotation follows (0-1, lower = smoother)
     rotationDamping: 0.8, // How much to dampen rotation (0-1, lower = more damping)
     // New configurable movement limits
-    maxMoveLeftPercent: 0.25, // How far thumbnail can move left (as % of container width)
-    maxMoveRightPercent: 0.25, // How far thumbnail can move right (as % of container width)
+    maxMoveLeftPercent: 0.1, // How far thumbnail can move left (as % of container width)
+    maxMoveRightPercent: 3, // How far thumbnail can move right (as % of container width)
     maxMoveUpPercent: 0.2, // How far thumbnail can move up (as % of container height)
     maxMoveDownPercent: 0.2, // How far thumbnail can move down (as % of container height)
     leftMovementMultiplier: 0.3, // Movement sensitivity when going left
     rightMovementMultiplier: 0.3, // Movement sensitivity when going right
-    verticalMovementMultiplier: 0.25 // Movement sensitivity for vertical movement
+    verticalMovementMultiplier: 0.4 // Movement sensitivity for vertical movement
   };
 
   // Mouse tracking variables
